34dd8d5ebc9346586447f6b3e446ef2daf6babdd
11165792 <sjdei@st.ug.edu.gh>
2024-06-01 21:02:42 +0000

    Created the task item component

---
+++ myApp/components/TaskItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const TaskItem = ({ task }) => (
+  <TouchableOpacity style={styles.task}>
+    <Text style={styles.taskText}>{task}</Text>
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  task: {
+    backgroundColor: '#fff',
+    padding: 60,
+    borderColor: '#e8d1ba',
+    borderWidth: 1,
+    borderRadius: 15,
+    marginBottom: 16,
+    elevation: 2,
+  },
+  taskText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
+
+export default TaskItem;
